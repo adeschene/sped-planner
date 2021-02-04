130a970cda65fca261725b154c708974f8f26043
adeschene <alecdeschene@gmail.com>
2021-02-03 19:55:12 -0800

    fix block bug, nix enum impl

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -1,17 +1,25 @@
 module ActivityHelper
-	BLOCKS = [:"9:30AM",:"10:30AM",:"11:30AM",:"Lunch",:"1:00PM",:"2:00PM",:"3:00PM"]
+	BLOCKS = [
+		"9:30AM",
+		"10:30AM",
+		"11:30AM",
+		"Lunch",
+		"1:00PM",
+		"2:00PM",
+		"3:00PM"
+	]
 
 	def isToday(dateToCheck)
   		return dateToCheck == Date.current ? "today" : ""
 	end
 
-	GETBLOCKCLASS = {
-		"9:30AM"  => "nine_thirty_block",
-		"10:30AM" => "ten_thirty_block",
-		"11:30AM" => "eleven_thirty_block",
-		"Lunch"   => "lunch_block",
-		"1:00PM"  => "one_block",
-		"2:00PM"  => "two_block",
-		"3:00PM"  => "three_block"
-	}
+	GETBLOCKCLASS = [
+		"nine_thirty_block",
+		"ten_thirty_block",
+		"eleven_thirty_block",
+		"lunch_block",
+		"one_block",
+		"two_block",
+		"three_block"
+	]
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,6 @@
 class Activity < ApplicationRecord
   has_many :notes, dependent: :destroy
 
-  enum block: [:'9:30AM', :'10:30AM', :'11:30AM', :Lunch, :'1:00PM', :'2:00PM', :'3:00PM']
-
   validates :title, :date, :block, presence: true
 
   def start_time
